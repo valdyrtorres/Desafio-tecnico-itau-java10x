73ac00a40fc8ff16a64bd41d6363fd14ebe373a0
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2026-05-17 15:08:46 -0300

    init: iniciando projeto do zero

---
+++ src/main/java/dev/java10x/itauJava10x/ItauJava10xApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.itauJava10x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauJava10xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauJava10xApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/_x/itauJava10x/ItauJava10xApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java._x.itauJava10x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauJava10xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
