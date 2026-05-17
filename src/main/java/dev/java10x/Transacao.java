fa2c010bad11a9e1b06502c21fe8ebba3aaf3a01
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2026-05-17 16:06:48 -0300

    feat: criacao do controller e de transacao

--- src/main/java/dev/java10x/Transacao.java
+++ src/main/java/dev/java10x/Transacao.java
@@ -1,4 +1,17 @@
 package dev.java10x;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transacao {
+
+    private BigDecimal valor;
+    private OffsetDateTime dataHora;
 }
