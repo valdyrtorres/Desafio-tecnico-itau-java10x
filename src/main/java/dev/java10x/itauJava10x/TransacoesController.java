631bdf10c670d5363894ba47d632b7c307a2e74e
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2026-06-04 22:06:33 -0300

    feat: swagger incluido

--- src/main/java/dev/java10x/itauJava10x/TransacoesController.java
+++ src/main/java/dev/java10x/itauJava10x/TransacoesController.java
@@ -1,10 +1,12 @@
 package dev.java10x.itauJava10x;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/transacao")
 public class TransacoesController {
@@ -17,15 +19,18 @@ public class TransacoesController {
 
     @PostMapping
     public ResponseEntity adicionar(@RequestBody TransacaoRequest transacaoRequest) {
+        log.info("Enviando requisicao de transacao para o servidor: ");
 
         try {
             transacaoService.validarTransacao(transacaoRequest);
             transacaoRepository.salvarDados(transacaoRequest);
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch(IllegalArgumentException exception) {
+            log.error("Erro em uma ou mais validações, tente novamente");
             System.out.println(exception.getMessage());
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
         } catch(Exception e) {
+            log.error("Erro no servidor, tente novamente");
             System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
@@ -34,6 +39,7 @@ public class TransacoesController {
 
     @DeleteMapping
     public ResponseEntity deletar() {
+        log.info("Lista de transacoes deletada: ");
         transacaoRepository.deletarDados();
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
--- src/main/java/dev/java10x/itauJava10x/estatistica/EstatisticaController.java
+++ src/main/java/dev/java10x/itauJava10x/estatistica/EstatisticaController.java
@@ -1,6 +1,7 @@
 package dev.java10x.itauJava10x.estatistica;
 
 import dev.java10x.itauJava10x.TransacaoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
 
+@Slf4j
 @RestController
 @RequestMapping("/estatistica")
 public class EstatisticaController {
@@ -24,6 +26,10 @@ public class EstatisticaController {
     @GetMapping
     public ResponseEntity estatistica() {
 
+        // Log de requisicao criado via lombok
+        log.info("Calculando estatisticas de transações: ");
+
+        // Calcular quantas transacoes acontecem em N segundos
         final var horaInicial = OffsetDateTime
                 .now()
                 .minusSeconds(estatisticaProperties.segundos());
